ffb96d663d0634f5c30549615b892bd18d6d89f1
Nabeel Mustafa <nabeelmustafaskp@gmail.com>
2024-12-19 20:44:28 +0500

    Set up folder structure and created Sidebar, UsersPage, and ProductsPage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router";
+import UsersPage from "./pages/UsersPage";
+import ProductsPage from "./pages/ProductsPage";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
-  return <div className="bg-black">App</div>;
+  return (
+    <div className="flex">
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<UsersPage />}></Route>
+        <Route path="products" element={<ProductsPage />}></Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
